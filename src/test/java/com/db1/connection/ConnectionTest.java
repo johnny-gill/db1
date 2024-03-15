15ea0a6aec7d7f2106e034ed3568968fce4993aa
johnny-gill <uint777@gmail.com>
2024-03-15 17:40:42 +0900

    add DriverManagerDataSource

---
+++ src/test/java/com/db1/connection/ConnectionTest.java
@@ -0,0 +1,39 @@
+package com.db1.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static com.db1.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("con1={}, class={}", con1, con1.getClass());
+        log.info("con2={}, class={}", con2, con2.getClass());
+    }
+
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
+        log.info("con1={}, class={}", con1, con1.getClass());
+        log.info("con2={}, class={}", con2, con2.getClass());
+    }
+
+}
+

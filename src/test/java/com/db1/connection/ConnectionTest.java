ee4978460679906bfbcb4eff64e3103fa09c99c7
johnny-gill <uint777@gmail.com>
2024-03-18 17:41:42 +0900

    hikari test

--- src/test/java/com/db1/connection/ConnectionTest.java
+++ src/test/java/com/db1/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package com.db1.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -35,5 +36,17 @@ public class ConnectionTest {
         log.info("con2={}, class={}", con2, con2.getClass());
     }
 
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
 }
 
